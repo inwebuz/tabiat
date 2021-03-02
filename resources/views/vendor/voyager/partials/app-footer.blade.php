6195d22b7bfe7bf7231339ff0060df7e89633e43
Ulugbek Yusupxodjayev <ulugbek.yu@gmail.com>
2021-03-02 11:59:14 +0500

    Admin footer fix

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -3,7 +3,7 @@
         @if (rand(1,100) == 100)
             <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
         @else
-            {!! __('voyager::theme.footer_copyright') !!} <a href="http://smartweb.uz" target="_blank">SmartWeb</a>
+            {!! __('voyager::theme.footer_copyright') !!} <a href="http://inweb.uz" target="_blank">InWeb</a>
         @endif
     </div>
 </footer>
